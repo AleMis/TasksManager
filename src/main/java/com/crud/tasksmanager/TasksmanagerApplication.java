5c8c1aa42a00fdf8c2a01381b4d9dfbd4ce394a4
AleMis <aleksander.miszkiewicz@gmail.com>
2017-11-24 23:20:35 +0100

    Tomcat deployment

--- src/main/java/com/crud/tasksmanager/TasksmanagerApplication.java
+++ src/main/java/com/crud/tasksmanager/TasksmanagerApplication.java
@@ -2,11 +2,18 @@ package com.crud.tasksmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksmanagerApplication {
+public class TasksmanagerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksmanagerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TasksmanagerApplication.class);
 	}
 }
