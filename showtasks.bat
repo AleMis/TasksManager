b27190cbde0328bbf1dd0c4c830cbdd335d892f3
AleMis <aleksander.miszkiewicz@gmail.com>
2017-11-25 10:28:36 +0100

    Shell command finished

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+call runcrud
+if "%ERRORLEVEL%" == "0" goto runchrome
+echo Something goes wrong
+goto fail
+
+:runchrome
+timeout 5
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+echo Cannot open chrome browser
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
