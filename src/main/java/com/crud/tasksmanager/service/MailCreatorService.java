27390e59b1735c4c3c1892c98fff2e50c86bf4c5
AleMis <aleksander.miszkiewicz@gmail.com>
2018-01-10 21:07:59 +0100

    MailService was developed

--- src/main/java/com/crud/tasksmanager/service/MailCreatorService.java
+++ src/main/java/com/crud/tasksmanager/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class MailCreatorService {
@@ -19,6 +22,12 @@ public class MailCreatorService {
     private ITemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://alemis.github.io/");
@@ -26,6 +35,10 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye","Best regards!" );
         context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
